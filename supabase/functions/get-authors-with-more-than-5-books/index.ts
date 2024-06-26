af26afbea60406c168be83b4de7d610af991fc8a
Mohammadreza <mdrazamani@gmail.com>
2024-06-26 18:24:41 +0330

    add get-authors-with-more-than-5-books fn

---
+++ supabase/functions/get-authors-with-more-than-5-books/index.ts
@@ -0,0 +1,23 @@
+import { serve } from 'https://deno.land/std@0.114.0/http/server.ts';
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.0.5';
+
+const supabase = createClient('https://qfqweyetibfyetcsshqs.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFmcXdleWV0aWJmeWV0Y3NzaHFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTk0MDU0MjMsImV4cCI6MjAzNDk4MTQyM30.z90xCrQfGfLGkF58Y_CWwKEJFr2PXiLpzFfNFlPp-ik');
+
+serve(async (req) => {
+  try {
+    const { data, error } = await supabase
+      .rpc('get_authors_with_more_than_5_books'); 
+
+    if (error) {
+      throw error;
+    }
+
+    return new Response(JSON.stringify(data), {
+      headers: { 'Content-Type': 'application/json' },
+    });
+  } catch (error: any) {
+    console.error('Error:', error.message);
+    return new Response(JSON.stringify({ error: error.message }), { status: 400 });
+  }
+});
+
